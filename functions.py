f2a30d6e52490bc54639840048f230da67f10666
BohBOhTN <Baha.etc@gmail.com>
2023-03-05 21:44:27 +0100

    Adding get_data_from_api() function

--- functions.py
+++ functions.py
@@ -8,3 +8,13 @@ import boto3
 
 link_api = 'https://app.nanonets.com/api/v2/OCR/Model/8e075f0c-ba58-451c-8f28-af3f1fda66d9/LabelFile/?async=false'
 file_path = './images/Screenshot_20230305_190131_iit.jpg'
+
+##CALLING THE API FUNCTION
+
+
+def get_data_from_api(link,path):
+    url = link
+    data = {'file': open(path, 'rb')}
+    response = requests.post(url, auth=requests.auth.HTTPBasicAuth('51d0a0da-ba9d-11ed-a27e-22a1d9fde453', ''), files=data)
+    return response
+
